4912520d056a9b62a8ee834f804a75df4e45f41c
Marcus Vinicius <marcusavanso06@gmail.com>
2026-04-01 12:15:43 -0300

    link dos botoes

--- src/compenents/Button.jsx
+++ src/compenents/Button.jsx
@@ -2,11 +2,15 @@ import style from './Button.module.css'
 import whatsapp from '../assets/svg/whatsapp.svg';
 
 const Button = () => {
+  const handleClick = () => {
+    window.open('https://wa.me/5514988151281', '_blank');
+  };
+
   return (
-    <button className={style.buttonRed}>
-      Entrar em contato   
+    <button className={style.buttonRed} onClick={handleClick}>
+      Entrar em contato    
       <span className={style.iconCircle}>
-        <img src={whatsapp} alt="Seta para a direita" />
+        <img src={whatsapp} alt="WhatsApp" />
       </span>
     </button>
   )
--- src/compenents/ButtonRed.jsx
+++ src/compenents/ButtonRed.jsx
@@ -2,9 +2,13 @@ import style from './ButtonRed.module.css';
 import arrow from '../assets/svg/arrow.svg';
 
 const ButtonRed = () => {
+  const handleClick = () => {
+    window.open('https://wa.me/5514988151281', '_blank');
+  };
+
   return (
-    <button className={style.buttonRed}>
-      Adiquirir carne
+    <button className={style.buttonRed} onClick={handleClick}>
+      Adquirir carne
       <span className={style.iconCircle}>
         <img src={arrow} alt="Seta para a direita" />
       </span>
